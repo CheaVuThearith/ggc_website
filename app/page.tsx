307bd2e1b2e764b35e02b865b43713672037c500
CheaVuThearith <cheavuthearith0@gmail.com>
2024-07-29 08:32:10 +0700

    blogpost too big

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,9 @@
-import ContactCard from "@/components/HomePage/ContactCard";
+import BlogSection from "@/components/HomePage/Sections/BlogSection";
 import ContactSection from "@/components/HomePage/Sections/ContactSection";
 import HeroSection from "@/components/HomePage/Sections/HeroSection";
-import RecyclingFacilitiesSection from "@/components/HomePage/Sections/RecyclingFacilitiesSection";
-import Navbar from "@/components/Navbar";
-import React from "react";
-import MissionCard from "@/components/HomePage/MissionCard";
-import BlogCard from "@/components/HomePage/BlogCard";
 import MissionSection from "@/components/HomePage/Sections/MissionSection";
-import BlogSection from "@/components/HomePage/Sections/BlogSection";
 import PartnersSeciton from "@/components/HomePage/Sections/PartnersSeciton";
+import RecyclingFacilitiesSection from "@/components/HomePage/Sections/RecyclingFacilitiesSection";
 
 export default function Home() {
   return (
