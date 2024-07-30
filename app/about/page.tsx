9a9e77fa04038538cc5b9ee4a36c72c7e77aa5bc
CheaVuThearith <cheavuthearith0@gmail.com>
2024-07-30 11:59:08 +0700

    fixed deployment error

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,24 +1,40 @@
-import React from 'react'
-import Section from '@/components/Section'
-import MissionSection from '@/components/HomePage/Sections/MissionSection'
-import Image from 'next/image'
+import React from "react";
+import Section from "@/components/Section";
+import MissionSection from "@/components/HomePage/Sections/MissionSection";
+import Image from "next/image";
 
-type Props = {}
+type Props = {};
 
 const page = (props: Props) => {
   return (
     <>
-      <Section title='About Us' > 
-        <p>Welcome to Green Growth Cycle, Phnom Penh’s top resource for recycling! We aim to create a greener future by connecting residents and businesses with trusted recycling services and manufacturers. Discover how easy it is to recycle responsibly and support a cleaner city with us!</p>
+      <Section title="About Us">
+        <p>
+          Welcome to Green Growth Cycle, Phnom Penh’s top resource for
+          recycling! We aim to create a greener future by connecting residents
+          and businesses with trusted recycling services and manufacturers.
+          Discover how easy it is to recycle responsibly and support a cleaner
+          city with us!
+        </p>
       </Section>
-      <MissionSection/>
-      <Section title='What We Do'>
-        <p>We are advancing the 3Rs—Reuse, Reduce, Recycle—in Phnom Penh through targeted education and community initiatives to foster eco-friendly practices and enhance waste management. Furthermore, we support local recycling manufacturers to streamline recycling processes and make it more accessible for residents.</p>
+      <MissionSection />
+      <Section title="What We Do">
+        <p>
+          We are advancing the 3Rs—Reuse, Reduce, Recycle—in Phnom Penh through
+          targeted education and community initiatives to foster eco-friendly
+          practices and enhance waste management. Furthermore, we support local
+          recycling manufacturers to streamline recycling processes and make it
+          more accessible for residents.
+        </p>
       </Section>
-    <Image src='/assets/recycleBanner.png' width={1500} height={480}/>
-
+      <Image
+        src="/assets/recycleBanner.png"
+        width={1500}
+        height={480}
+        alt="/assets/recycleBanner.png"
+      />
     </>
-  )
-}
+  );
+};
 
-export default page
+export default page;
