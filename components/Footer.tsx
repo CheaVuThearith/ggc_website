a1a0b733a7e5be8665512ac0af619e0177f3b0ba
Tai-NgonHeng <heng68807@gmail.com>
2024-07-31 09:06:09 +0700

    re-sizing footer Icon

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,12 +3,12 @@ import React from 'react';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-gray-50 py-8">
-      <div className="container mx-auto px-4">
-        <div className="flex justify-between items-center mt-8">
-          <div className="flex items-center space-x-80">
+    <footer className="bg-gray-50 py-4">
+      <div className="container mx-auto px-4 mt-16">
+        <div className="flex justify-between items-center">
+          <div className="flex items-center space-x-4">
             <img src="/assets/icons/Logo.png" alt="Green Growth Cycle Logo" className="h-20" />
-            <span className="text-gray-500 ml-7">© 2024 Green Growth Cycle</span>
+            <span className="text-gray-500">© 2024 Green Growth Cycle</span>
           </div>
           <div className="flex space-x-4">
             <a href="https://www.facebook.com/profile.php?id=61561837272386" target='_blank' className="text-gray-500 hover:text-green-700">
