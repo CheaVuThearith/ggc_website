e18b7324b6746e67b1c83181bd7e28875341be4a
CheaVuThearith <cheavuthearith0@gmail.com>
2024-08-06 11:18:14 +0700

    recycling page done

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 // pages/index.tsx
-import React from 'react';
+import React from "react";
 import BlogSection from "@/components/HomePage/Sections/BlogSection";
 import ContactSection from "@/components/HomePage/Sections/ContactSection";
 import HeroSection from "@/components/HomePage/Sections/HeroSection";
@@ -7,6 +7,7 @@ import MissionSection from "@/components/HomePage/Sections/MissionSection";
 import PartnersSeciton from "@/components/HomePage/Sections/PartnersSeciton";
 import RecyclingFacilitiesSection from "@/components/HomePage/Sections/RecyclingFacilitiesSection";
 import RecyclingFacilitiesCard from "@/components/RecyclePage/RecyclingFacilitiesCard";
+import RecyclingFacilitiesCardExpanded from "@/components/RecyclePage/RecyclingFacilitiesCardExpanded";
 
 export default function Home() {
   return (
@@ -17,12 +18,7 @@ export default function Home() {
       <PartnersSeciton />
       <BlogSection />
       <ContactSection />
-      <div className="container mx-auto px-4 py-8">
-        <h1 className="text-2xl font-bold mb-4 text-center"></h1>
-        <div className="flex flex-wrap justify-center gap-4">
-          <RecyclingFacilitiesCard title="Hello Everybody" location="teas nh" hours="8:00 am to 6:00 pm" material="money" opennow={true} />
-        </div>
-      </div>
+      
     </>
   );
 }
--- components/RecyclePage/RecyclingFacilitiesCard.tsx
+++ components/RecyclePage/RecyclingFacilitiesCard.tsx
@@ -1,30 +1,108 @@
-import React from 'react';
-import { FaMapMarkerAlt, FaClock, FaRecycle } from 'react-icons/fa';
-
-type Props = { title: string; location: string; hours: string; material: string; opennow: boolean }
-const RecyclingFacilitiesCard = ({ title, location, hours, material, opennow }: Props) => {
+"use client";
+import { AnimatePresence, motion } from "framer-motion";
+import Image from "next/image";
+import React, { useState } from "react";
+import { FaMapMarkerAlt, FaClock, FaRecycle } from "react-icons/fa";
+type Props = {
+  title: string;
+  location: string;
+  typesOfWaste: string[];
+  startHour: Date;
+  endHour: Date;
+  image: string;
+  onClick: () => void;
+  layoutId: string;
+};
+const RecyclingFacilitiesCard = ({
+  onClick,
+  title,
+  location,
+  typesOfWaste,
+  startHour,
+  endHour,
+  image,
+  layoutId,
+}: Props) => {
+  const openStatus = false;
   return (
-    <div className="border rounded-lg shadow-md w-96 p-6 relative">
-      {opennow && <span className="bg-green-500 text-white px-2 py-1 rounded-full text-sm absolute top-2 left-2">Open now!</span>}
-      <div className="flex flex-col items-center mb-2">
-        <img src="assets/facilities images/altruistic traveller.png" alt={title} className="mb-4" />
-        <div className="text-center">
-          <h3 className="text-lg font-bold mb-2">{title}</h3>
-          <div className="flex items-center text-gray-600 mb-1">
-            <FaMapMarkerAlt className="mr-1" />
-            <p>{location}</p>
-          </div>
-          <div className="flex items-center text-gray-600 mb-1">
-            <FaClock className="mr-1" />
-            <p>{hours}</p>
-          </div>
-          <div className="flex items-center text-gray-600">
-            <FaRecycle className="mr-1" />
-            <p>{material}</p>
+    <AnimatePresence>
+      <motion.div
+        onClick={onClick}
+        className="relative rounded-lg border p-6 shadow-md"
+        layoutId={layoutId}
+      >
+        {/* open status */}
+        <span
+          className={`${openStatus ? "bg-primary" : "bg-red-600"} absolute left-2 top-2 rounded-full px-2 py-1 text-sm text-white`}
+        >
+          {openStatus ? "Open Now!" : "Closed!"}
+        </span>
+
+        <div className="flex flex-col gap-x-6 gap-y-3">
+          {/* img */}
+          <motion.div layoutId={layoutId + "image"} layout="position">
+            <Image
+              src={image}
+              alt={title}
+              height={300}
+              width={350}
+              style={{
+                objectFit: "cover",
+                maxWidth: "350px",
+                maxHeight: "300px",
+              }}
+              className="rounded-md object-cover"
+            />
+          </motion.div>
+
+          {/* text info */}
+          <div className="flex flex-col gap-y-4">
+            <motion.h2
+              layoutId={layoutId + "title"}
+              layout="position"
+              className="text-xl font-bold"
+            >
+              {title}
+            </motion.h2>
+            {/* extra info */}
+            <motion.div
+              layoutId={layoutId + "info"}
+              layout="position"
+              className="flex flex-col gap-y-2"
+            >
+              <div className="flex items-center gap-x-2 text-gray-600">
+                <FaMapMarkerAlt className="" />
+                <p>{location}</p>
+              </div>
+              <div className="flex items-center gap-x-2 text-gray-600">
+                <FaClock className="" />
+                <p>
+                  {new Date(startHour).toLocaleTimeString([], {
+                    hour: "2-digit",
+                    minute: "2-digit",
+                  })}{" "}
+                  -{" "}
+                  {new Date(endHour).toLocaleTimeString([], {
+                    hour: "2-digit",
+                    minute: "2-digit",
+                  })}
+                </p>
+              </div>
+              <div className="flex items-center gap-x-2 text-gray-600">
+                <FaRecycle className="" />
+                <p>
+                  {typesOfWaste
+                    .map(
+                      (waste) => waste.charAt(0).toUpperCase() + waste.slice(1)
+                    )
+                    .join(", ")}
+                </p>
+              </div>
+            </motion.div>
           </div>
         </div>
-      </div>
-    </div>
+      </motion.div>
+    </AnimatePresence>
   );
 };
 
